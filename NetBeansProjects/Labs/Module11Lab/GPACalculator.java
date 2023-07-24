6b8da43dba346004c100a071593a1fdfa57fcade
Ash <ash@mountaininteractive.com>
2023-07-24 13:37:00 -0400

    Update GPACalculator.java

--- NetBeansProjects/Labs/Module11Lab/GPACalculator.java
+++ NetBeansProjects/Labs/Module11Lab/GPACalculator.java
@@ -1,6 +1,7 @@
 package NetBeansProjects.Labs.Module11Lab;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class GPACalculator {
 
@@ -8,55 +9,57 @@ public class GPACalculator {
         Scanner scnr = new Scanner(System.in);
         char letterGrade;    // User defined letter grade 
         int creditHours;     // User defined number of credit hours
-        char quitCmd;        // Indicates quit/continue
-
-        quitCmd = 'a';
+        char quitCmd = 'a';  // Indicates quit/continue
 
         int qualityPoints = 0;
         int totalCreditHrs = 0;
 
         while (quitCmd != 'q') {
+            try {
+                // Get user data
+                System.out.print("Enter letter grade: ");
+                letterGrade = scnr.nextLine().charAt(0);
 
-            // Get user data
-            System.out.print("Enter letter grade: ");
-            letterGrade = scnr.nextLine().charAt(0);
+                // Error checking, unknown letter grade
+                if (getLetterGradeValue(letterGrade) == -1) {
+                    throw new Exception("Invalid letter grade.");
+                }
 
-            if (isValidLetterGrade(letterGrade)) {
                 System.out.print("Enter number of credit hours: ");
                 creditHours = scnr.nextInt();
-                scnr.nextLine(); // Consume the newline character
+                scnr.nextLine();  // Consume newline left-over
 
-                if (isValidCreditHours(creditHours)) {
-                    totalCreditHrs += creditHours;
-                    qualityPoints += getLetterGradeValue(letterGrade) * creditHours;
-
-                    System.out.print("\nEnter any key to continue entering grade or 'q' to quit: ");
-                    quitCmd = scnr.nextLine().charAt(0);
-                } else {
-                    System.out.println("Invalid number of credit hours. Try again!");
+                // Error checking, negative credit hours
+                if (creditHours < 0) {
+                    throw new Exception("Invalid number of credit hours. Try again!");
                 }
-            } else {
-                System.out.println("Invalid letter grade. Try again!");
+
+                totalCreditHrs += creditHours;
+                qualityPoints += getLetterGradeValue(letterGrade) * creditHours;
+            } catch (InputMismatchException ime) {
+                // Prints a custom error message
+                System.out.println("Invalid input. Please enter an integer for credit hours.");
+                // Consume the invalid token
+                scnr.nextLine();
+            } catch (Exception excpt) {
+                // Prints the error message passed by throw statement
+                System.out.println(excpt.getMessage());
+                System.out.println("Cannot compute GPA");
             }
+
+            System.out.print("\nEnter any key to continue entering grade or 'q' to quit: ");
+            quitCmd = scnr.nextLine().charAt(0);
         }
 
         // Calculate GPA - Divide the total quality points by the total credit hours.
-        double gpa = computeGPA(qualityPoints, totalCreditHrs);
-
-        // Print user GPA info
-        System.out.println("GPA: " + gpa);
-    }
-
-    private static boolean isValidLetterGrade(char letterGrade) {
-        return (letterGrade == 'a' || letterGrade == 'A' ||
-                letterGrade == 'b' || letterGrade == 'B' ||
-                letterGrade == 'c' || letterGrade == 'C' ||
-                letterGrade == 'd' || letterGrade == 'D' ||
-                letterGrade == 'f' || letterGrade == 'F');
-    }
-
-    private static boolean isValidCreditHours(int creditHours) {
-        return creditHours > 0;
+        try {
+            double gpa = computeGPA(qualityPoints, totalCreditHrs);
+            // Print user GPA info
+            System.out.println("GPA: " + gpa);
+        } catch (ArithmeticException excpt) {
+            System.out.println(excpt.getMessage());
+            System.out.println("Illegal operation. Cannot compute GPA.");
+        }
     }
 
     private static int getLetterGradeValue(char letterGrade) {
@@ -80,6 +83,10 @@ public class GPACalculator {
     }
 
     private static double computeGPA(int qualityPoints, int creditHours) {
+        if (creditHours == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
         return ((double) qualityPoints) / creditHours;
     }
+    
 }
